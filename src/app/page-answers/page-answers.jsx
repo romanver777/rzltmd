811ff01b03eda5a49b7e5a211b6f650353c2105c
romanver <romanver777@yandex.ru>
2024-05-07 13:52:50 +0300

    add cta

--- src/app/page-answers/page-answers.jsx
+++ src/app/page-answers/page-answers.jsx
@@ -1,7 +1,7 @@
 import PageLayout from "../../components/page-layout/page-layout";
 import Header from "../../components/header/header";
-// import Content from "../../components/content/content";
-// import Cta from "../../components/cta/cta";
+import Content from "../../components/content/content";
+import Cta from "../../components/cta/cta";
 // import Answers from "../../components/answers/answers";
 // import Footer from "../../components/footer/footer";
 
@@ -9,11 +9,10 @@ const PageAnswers = () => {
   return (
     <PageLayout>
       <Header />
-      {//<Content>
-        //<Cta />
-        //<Answers />
-      //</Content>
-      //<Footer />
+      <Content>
+        <Cta />
+      </Content>
+      {//<Footer />
     }
     </PageLayout>
   );
